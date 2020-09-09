674433a9619962e27b193f3db90803f0a1cf2071
dittons <38831995+dittons@users.noreply.github.com>
2020-09-09 11:23:13 +0100

    Update calculate_response_code.c

--- calculate_response_code.c
+++ calculate_response_code.c
@@ -10,5 +10,5 @@ uint16_t calculate_response_code( uint16_t challenge )
 	int n = challenge % 11;
 	uint16_t m = (challenge << n) | (challenge >> (16 - n));
 	uint16_t x = ((challenge + 38550) ^ m);
-	return x % 65336; // this value is correct (perhaps a typo for 65536 in the originial?)
+	return x % 65336; // this value is correct (perhaps a typo for 65536 in the original?)
 }
